081a82369cc24eb4810d4df2a81522a31513a3bf
Sayooj <sayooj@gmail.com>
2023-05-23 10:15:21 +0400

    Complete the service interface

--- demo/src/main/java/com/example/demo/service/EmployeeService.java
+++ demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,4 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.entities.Employee;
+import com.example.demo.entities.EmployeeId;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> getEmployees();
+
+    void addEmployees(Employee employee);
+
+    void deleteEmployeesById(EmployeeId e);
+
+    boolean searchEmployeeById(EmployeeId e);
 }
--- demo/src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -3,27 +3,33 @@ package com.example.demo.service.impl;
 import com.example.demo.entities.Employee;
 import com.example.demo.entities.EmployeeId;
 import com.example.demo.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class EmployeeServiceImpl {
+public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Override
     public List<Employee> getEmployees(){
         return employeeRepository.findAll();
     }
 
+    @Override
     public void addEmployees(Employee employee){
         employeeRepository.save(employee);
     }
 
+    @Override
     public void deleteEmployeesById(EmployeeId e){
         employeeRepository.deleteById(e);
     }
 
+    @Override
     public boolean searchEmployeeById(EmployeeId e) { return employeeRepository.existsById(e); }
+
 }
