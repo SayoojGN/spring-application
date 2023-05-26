6c0629c3714df630bbc17a3fb504ada0ddcf6472
Sayooj <sayooj@gmail.com>
2023-05-26 11:35:11 +0400

    Few chanegs to set up swagger-ui

--- demo/src/main/java/com/example/demo/controllers/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -2,34 +2,40 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.Employee;
 import com.example.demo.entities.EmployeeId;
+import com.example.demo.models.EmployeeDTO;
 import com.example.demo.service.impl.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
-import com.example.demo.models.EmployeeDTO;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@RequestMapping("/")
+@RequestMapping("/swagger-ui/index.html")
 @RestController
 public class EmployeeController {
     @Autowired
     private EmployeeServiceImpl service;
 
-    @GetMapping
+    @GetMapping("/")
     public List<Employee> getEmployee() {
         return service.getEmployees();
     }
 
-    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @GetMapping("/get")
+    public Employee getEmployeesById(@RequestParam("empId") Long empId, @RequestParam("companyCode") Long companyCode){
+        EmployeeId temp2 = new EmployeeId(empId, companyCode);
+        Employee temp = service.getEmployeesById(temp2);
+        temp.setEmiratesIdNo(service.decrypt(temp.getEmiratesIdNo()));
+        return temp;
+    }
 
     @GetMapping("/{empId}/{companyCode}")
-    public Employee getEmployeesById(@PathVariable("empId") Long empId, @PathVariable("companyCode") Long companyCode){
+    public Employee getEmployeesById1(@PathVariable("empId") Long empId, @PathVariable("companyCode") Long companyCode){
         EmployeeId temp2 = new EmployeeId(empId, companyCode);
         Employee temp = service.getEmployeesById(temp2);
         temp.setEmiratesIdNo(service.decrypt(temp.getEmiratesIdNo()));
@@ -136,7 +142,7 @@ public class EmployeeController {
         }
     }
 
-    @DeleteMapping
+    @DeleteMapping("/delete")
     public String deleteEmployees(@RequestBody EmployeeDTO e) {
         EmployeeId temp = new EmployeeId();
         temp.setEmpId(e.getEmpId());
@@ -144,11 +150,19 @@ public class EmployeeController {
         service.deleteEmployeesById(temp);
         return "Deleted Successfully";
     }
-    @DeleteMapping("/{empId}/{companyCode}")
-    public String deleteEmployees(@PathVariable("empId") Long empId, @PathVariable("companyCode") Long companyCode){
+
+    @DeleteMapping("/delete2")
+    public String deleteEmployees(@RequestParam("empId") Long empId, @RequestParam("companyCode") Long companyCode){
         EmployeeId temp = new EmployeeId(empId, companyCode);
         service.deleteEmployeesById(temp);
         return "Delete Successfully";
+    }
+
+    @DeleteMapping("/{empId}/{companyCode}")
+    public String deleteEmployees2(@PathVariable("empId") Long empId, @PathVariable("companyCode") Long companyCode) {
+        EmployeeId temp = new EmployeeId(empId, companyCode);
+        service.deleteEmployeesById(temp);
+        return "Delete Successful";
     }
 
 }
